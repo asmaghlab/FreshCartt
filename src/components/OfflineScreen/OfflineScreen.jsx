f54f755df0812e0d8c65a6ccba494a6e75454f55
Asmaa Adel <asmaghlab073@gmail.com>
2026-04-06 05:27:04 +0200

    save

--- src/components/OfflineScreen/OfflineScreen.jsx
+++ src/components/OfflineScreen/OfflineScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import useOnLineStatus from '../../hooks/useOnLineStatus.jsx';
+import useOnLineStatus from '../../Hooks/useOnLineStatus.jsx';
 import { Wifi, CircleAlert, RefreshCw, Search } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
